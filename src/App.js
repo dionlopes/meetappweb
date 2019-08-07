459e8523e94eb44b8de2c27ee8e6b9b6abbc053f
Dion <dion.mello@hotmail.com>
2019-08-07 16:56:09 -0300

    chore: Configurando Redux e Saga

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
+import './config/ReactotronConfig';
 
 import Routes from './routes';
 import History from './services/history'
 
-import CreateGlobalStyle from './styles/global';
+import store from './store'
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
+    <Provider store={store}>
     <Router history={History}>
       <Routes />
-      <CreateGlobalStyle />
+      <GlobalStyle />
     </Router>
+    </Provider>
   );
 }
 
