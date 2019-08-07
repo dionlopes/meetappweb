dcfdd8b260a3a046c8afbd284cad1a5a7b12343c
Dion <dion.mello@hotmail.com>
2019-08-07 15:13:34 -0300

    chore: Rotas privadas e Layout por página

---
+++ src/routes/Route.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
+import PublicLayout from '~/pages/_layouts/public';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate = false,
+  screenLoginRegister = false,
+  ...rest
+}) {
+
+  const signed = false;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && screenLoginRegister) {
+    return <Redirect to="/" />;
+  }
+
+  let Layout = screenLoginRegister ? AuthLayout : null;
+
+  if (!Layout && !isPrivate) {
+    Layout = signed ? DefaultLayout: AuthLayout;
+  }
+
+  if (!Layout && !isPrivate) {
+    Layout = PublicLayout;
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  )
+}
