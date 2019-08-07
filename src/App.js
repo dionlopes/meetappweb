896820383831cd015f2d1087013c5e9359e08cc4
Dion <dion.mello@hotmail.com>
2019-08-07 13:11:10 -0300

    feat: css global

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import History from './services/history'
 
+import CreateGlobalStyle from './styles/global';
+
 function App() {
   return (
     <Router history={History}>
       <Routes />
+      <CreateGlobalStyle />
     </Router>
   );
 }
---
+++ src/styles/global.js
@@ -0,0 +1,62 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+    min-width: 600px;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+    color: #fff;
+  }
+
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    color: #ffffff;
+    font-size: 16px;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+    background-color: #e5556e !important;
+  }
+
+  button#voltar {
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 25px 55px;
+    width: 71px;
+    height: 42px;
+    border-radius: 4px;
+    border: none;
+    a{
+      width: 31px;
+      height: 18px;
+      font-weight: 700;
+    }
+  }
+`;
