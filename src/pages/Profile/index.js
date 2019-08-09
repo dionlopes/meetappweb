7b3189bcacb30444440a479d6f71d59b72be6fce
Dion <dion.mello@hotmail.com>
2019-08-09 16:50:35 -0300

    chore: Salvando dados do perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,27 +1,36 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { MdAddCircleOutline } from 'react-icons/md';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
   return (
     <Container>
-      <Form>
+      <Form initialData={profile} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" placeholder="Seu endereço de e-mail" />
 
         <hr />
         <Input
-          name="oldPassword"
+          name="old_password"
           type="password"
           placeholder="Sua senha atual"
         />
         <Input type="password" name="password" placeholder="Nova senha" />
         <Input
           type="password"
-          name="confirmPassword"
+          name="password_confirmation"
           placeholder="Confirmação de senha"
         />
 
